b57e7852661956ddb3596b70a56e3ce60725ae2d
MaicFdz <maic_fdz@outlook.com>
2020-10-29 07:32:32 -0300

    nuevoCommit

--- subir_a_GitHub.py
+++ subir_a_GitHub.py
@@ -9,7 +9,7 @@ os.system('git add .')
 
 os.system('git commit --m "nuevoCommit"')
 
-os.system('git remote add origin http://github.com/MaicFdz/Calculadora')
+os.system('git remote add origin https://github.com/MaicFdz/Calculadora')
 
 os.system('git push origin main')
 
